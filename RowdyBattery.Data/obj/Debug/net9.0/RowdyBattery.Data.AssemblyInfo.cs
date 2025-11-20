afab55834381f991eea1f6205ca36795bd2dc315
Jessica Chen <zixin828@gmail.com>
2025-11-20 18:46:55 -0500

    Lab 6 - Securing the Web and API
    Updated project and NuGet cache files to use consistent uppercase drive letters and backslashes in Windows paths. No functional code changes were made; this improves consistency and may help avoid path-related issues on some systems.

--- RowdyBattery.Data/obj/Debug/net9.0/RowdyBattery.Data.AssemblyInfo.cs
+++ RowdyBattery.Data/obj/Debug/net9.0/RowdyBattery.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RowdyBattery.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66bd8ca78e2c32c917a59b97a798208984781b4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16fab537d227a5c549e3c66c4ad3f1bc7d9a752d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RowdyBattery.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RowdyBattery.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
